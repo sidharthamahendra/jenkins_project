2c47b2ef8d57e20d72f49bf78d51335b45329202
sidharthamahendra <151663779+sidharthamahendra@users.noreply.github.com>
2025-11-30 20:02:14 +0530

    Add Login class with login method

---
+++ login.java
@@ -0,0 +1,7 @@
+// login.java
+public class Login {
+    public static boolean login(String user, String pass) {
+        System.out.println("Login success for: " + user);
+        return true;
+    }
+}
